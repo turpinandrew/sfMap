dce6695fdf16f748d0052652f861b489e11c7152
Andrew Turpin <aturpin@unimelb.edu.au>
2019-04-15 20:44:58 +1000

    fixed some flipping. Added a dot to end of raphe. Added shade VF when hover on sector. Added clickable 24-2. Todo: add g pattern. Add image load.

--- map.js
+++ map.js
@@ -20,6 +20,7 @@
 const scols = ["#00008F","#0000EA","#0047FF","#00A2FF","#00FEFF","#5AFFA5", "#B5FF4A","#FFED00","#FF9200","#FF3700","#DB0000","#800000"];
 
 var g_exploding_sector = -1;  // sector to explode out of ONH pie
+var g_highlighted_sector_in_vf = -1;  // sector to highlight in VF plot
 
 var g_cwidth = get_xmax();            // virtual pixels   (all scaling done relative to this)
 var g_cheight = get_ymax();
@@ -27,6 +28,7 @@ var g_cheight = get_ymax();
 const g_axis_width = 100;               // virtual pixels
 const g_d2p = 10;  // scaling factor to convert 1 degree into virtual pixels
 const g_radius_vf_loc = 15;   // virtual pixels
+const g_onh_key_radius = 11 * g_d2p; // virtual pixels
 
 const EYE_LEFT = 0
 const EYE_RIGHT = 1
@@ -36,13 +38,17 @@ var g_onhx = -15;     // always negative!!! (left eye)
 var g_onhy = 2;
 var g_raphe = 174;
 
+    // regions of the canvas
 var g_regions = [];   // array of (x,y,label)  (x,y) in pixels on actual current canvas (not 'virtual')
-                      // label is 0..11 for a sector, and then these
+                      // label is 0..11 for a sector for visual field points
+                      // label is 100..111 for a sector on the onh key/pie
+                      // and these...
 const REGION_EYE       = 12;
 const REGION_ONH       = 13;
 const REGION_RAPHE_END = 14;
 const REGION_ONHX      = 15;
 const REGION_ONHY      = 16;
+const REGION_PATTERN   = 17;
 
 const DRAG_NONE = 0
 const DRAG_ONH = 1
@@ -51,6 +57,10 @@ const DRAG_ONHX = 3
 const DRAG_ONHY = 4
 
 var g_dragging = DRAG_NONE;
+
+const PATTERN_242 = 0;
+const PATTERN_G = 1;
+var g_pattern = PATTERN_242;
 
     // Return Euclidean distance between image_points[i1] and image_points[i2]
 function distance(i1, i2) {
@@ -64,12 +74,12 @@ function get_centre_of_VF() {
     return [g_axis_width + 30*g_d2p + g_radius_vf_loc , g_cheight / 2 - g_axis_width/2]; 
 }
 
-    // draw sphere map for elements onhx, onhy, eye
-    // nodal_prop = proportion of axial length that is nodal length (eg 17/23 mm)
-function draw_canvas() {
-    resize_canvas('canvasImage', window.innerWidth, window.innerHeight);
+    // Draw the visual field locations colored by sector
+    // If g_highlighted_sector_in_vf > -1, then shade those points
+    // pass canvas context
+function draw_vf_locs(ctx) {
 
-             // for drawing the map
+             // left eye VF, right eye Retina
    const tfd2_locs = [         [9,-21], [3,-21], [-3,-21], [-9,-21], 
                      [15,-15], [9,-15], [3,-15], [-3,-15], [-9,-15], [-15,-15], 
             [21,-9], [15, -9], [9, -9], [3, -9], [-3, -9], [-9, -9], [-15, -9], [-21,-9], 
@@ -79,31 +89,41 @@ function draw_canvas() {
                      [15, 15], [9, 15], [3, 15], [-3, 15], [-9, 15], [-15, 15],
                                [9, 21], [3, 21], [-3, 21], [-9, 21]];
 
-
-    var can = document.getElementById('canvasImage')
-    var ctx = can.getContext('2d');
-    ctx.clearRect(0, 0, can.width, can.height);
-    //ctx.rect(0, 0, can.width, can.height);
-    //ctx.stroke();
-
-    const xm = g_eye == EYE_RIGHT ? 1 : -1 ; 
-    const mcols = map_cols[g_onhx + 18.0][g_onhy + 2.0][g_raphe -161];
     const [cx, cy] = get_centre_of_VF();
-
-    g_regions = [];  // reset regions
+    const xm = g_eye == EYE_LEFT ? 1 : -1 ; 
+    const mcols = map_cols[g_onhx + 18.0][g_onhy + 2.0][g_raphe -161];
 
         // draw VF
     const radius = scale(g_radius_vf_loc);
-    for (i = 0 ; i < tfd2_locs.length ; i++) {
+    for (var i = 0 ; i < tfd2_locs.length ; i++) {
         var x = scale(cx + xm * tfd2_locs[i][0] * g_d2p);
         var y = yscale(cy - tfd2_locs[i][1] * g_d2p);      // note - as (0,0) is top left
+
+        var sector = mcols[i]-1; // note -1 as javascript starts at 0
+        if (g_highlighted_sector_in_vf - 100 == sector) {
+            ctx.beginPath();
+            ctx.arc(x, y, 2*radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = "#cccccc";
+            ctx.strokeStyle = "#cccccc";
+            ctx.fill();
+            ctx.closePath();
+        }
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = scols[mcols[i]-1]; // note -1 as javascript starts at 0
+        ctx.strokeStyle = scols[sector];
+        ctx.fillStyle = scols[sector];
         ctx.fill();
+        ctx.closePath();
 
-        g_regions.push([x, y, mcols[i]-1]);
+        g_regions.push([x, y, sector]);
     }
+    ctx.stroke();
+}
+
+    // draw axes of VF and sliders for ONH x y adjustment
+function draw_axes(ctx) {
+    const xm = g_eye == EYE_LEFT ? 1 : -1 ; 
+    const [cx, cy] = get_centre_of_VF();
 
         // draw axes
     ctx.fillStyle = "#000000";
@@ -158,6 +178,12 @@ function draw_canvas() {
     ctx.stroke();
     ctx.strokeStyle = "black";
     g_regions.push([ttx, tty, REGION_ONHY]);
+}
+
+    // draw onh and raphe and fodi line
+function draw_onh_and_raphe(ctx) {
+    const xm = g_eye == EYE_LEFT ? 1 : -1 ; 
+    const [cx, cy] = get_centre_of_VF();
 
         // draw blind spot (onh) on VF (so y is cy +, not cy -)
     var x = scale(cx + xm * g_d2p * g_onhx);
@@ -170,7 +196,7 @@ function draw_canvas() {
         // draw raphe on VF (so y is cy +, not cy -)
     var onhAngle = Math.atan2(g_onhy, xm * g_onhx)*180/Math.PI;
     var rAngle = onhAngle - g_raphe;
-    if (g_eye == EYE_LEFT)
+    if (g_eye == EYE_RIGHT)
          rAngle = onhAngle + g_raphe;
 
     raphe_end_x = scale (cx + 30 * g_d2p * Math.cos(rAngle/180*Math.PI));
@@ -181,12 +207,21 @@ function draw_canvas() {
     ctx.moveTo(scale(cx), yscale(cy));      // line to onh
     ctx.lineTo(x,y);
     ctx.stroke();
+    ctx.beginPath();
+    ctx.arc (raphe_end_x, raphe_end_y, scale(4), 0, 2*Math.PI, true);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
 
     g_regions.push([x, y, REGION_ONH]);
     g_regions.push([raphe_end_x, raphe_end_y, REGION_RAPHE_END]);
+}
 
-        // draw ONH key
-    const onh_key_radius = scale(35 * g_d2p/3); // pixels
+    // draw ONH key
+function draw_onh_key(ctx) {
+    const [cx, cy] = get_centre_of_VF();
+
+    const onh_key_radius = scale(g_onh_key_radius);
     const onh_key_x = scale(g_axis_width + g_cwidth * 12 / 16);
     const onh_key_y = yscale(cy); 
 
@@ -209,7 +244,13 @@ function draw_canvas() {
         ctx.fillStyle = scols[i];
         ctx.fill();
         ctx.restore();
+
+        g_regions.push([xx + onh_key_radius/2*Math.cos(s+15/180*Math.PI), 
+                        yy + onh_key_radius/2*Math.sin(s+15/180*Math.PI), 
+                        100 + i]);
     }
+
+        // Add TSNIT annotations
     ctx.fillStyle = "black";
     if (g_eye == EYE_LEFT) {
         ctx.textAlign = "right";
@@ -230,12 +271,41 @@ function draw_canvas() {
     ctx.fillText("S" , onh_key_x, onh_key_y - 1.3*onh_key_radius);
     ctx.textBaseline="bottom"; 
     ctx.fillText("I", onh_key_x, onh_key_y + 1.3*onh_key_radius);
+}// draw_onh_key()
+
+
+    // draw sphere map for elements onhx, onhy, eye
+    // nodal_prop = proportion of axial length that is nodal length (eg 17/23 mm)
+function draw_canvas() {
+    resize_canvas('canvasImage', window.innerWidth, window.innerHeight);
+
+    var can = document.getElementById('canvasImage')
+    var ctx = can.getContext('2d');
+    ctx.clearRect(0, 0, can.width, can.height);
+    //ctx.rect(0, 0, can.width, can.height);
+    //ctx.stroke();
+
+    const xm = g_eye == EYE_LEFT ? 1 : -1 ; 
+    const [cx, cy] = get_centre_of_VF();
+
+    g_regions = [];  // reset regions
+
+    draw_vf_locs(ctx);
+    draw_axes(ctx);
+    draw_onh_and_raphe(ctx);
+    draw_onh_key(ctx);
 
         // draw Eye Label Region
     ctx.textBaseline="middle"; 
-    ctx.font = scale(40) + "px Arial";
+    ctx.font = scale(30) + "px Arial";
     ctx.fillText(g_eye == EYE_LEFT ? "OS" : "OD" , scale(g_axis_width + 50), yscale(50));
     g_regions.push([scale(100), yscale(50), REGION_EYE]);
+
+        // draw Pattern
+    ctx.textBaseline="middle"; 
+    ctx.font = scale(30) + "px Arial";
+    ctx.fillText(g_pattern == PATTERN_242 ? "24-2" : "G" , scale(g_axis_width + 50), yscale(100));
+    g_regions.push([scale(g_axis_width + 50), yscale(100), REGION_PATTERN]);
 
         // draw params
     ctx.textBaseline="middle"; 
@@ -244,6 +314,7 @@ function draw_canvas() {
     ctx.fillText(ss, scale(g_cwidth - 160), yscale(g_cheight - 50));
     ss = "DiFoRaphe = " + g_raphe;
     ctx.fillText(ss, scale(g_cwidth - 200), yscale(g_cheight - 20));
+
 }
 
 window.onresize = function() { draw_canvas(); } 
@@ -285,7 +356,7 @@ function set_new_raphe(x,y) {
     var x_deg = (unscale(x) - cx) / g_d2p;
     var y_deg = (yunscale(y) - cy) / g_d2p;
 
-    const xm = g_eye == EYE_LEFT ? -1 : 1;
+    const xm = g_eye == EYE_RIGHT ? -1 : 1;
     var fo_raphe = Math.atan2(y_deg,  xm * x_deg);   // for right eye
     var fodi     = Math.atan2(-g_onhy, -g_onhx);   
 
@@ -299,6 +370,7 @@ function set_new_raphe(x,y) {
 }
 
 // return [x,y,label,min_distance] for event e
+// min_distance is square of pixel distance in real pixels
 function get_xy_from_event(e) {
     var x = e.clientX;
     var y = e.clientY;
@@ -327,10 +399,13 @@ function handle_mousemove(e) {
     const [x, y, label, min_d] = get_xy_from_event(e);
 
     if (g_dragging == DRAG_NONE) {
-        if (0 <= label && label < 12 && min_d <= Math.pow(scale(g_radius_vf_loc),2)) {
+        if (0 <= label && label < 12 && min_d <= Math.pow(scale(2 * g_radius_vf_loc),2)) {
             g_exploding_sector = label;
+        } else if (label >= 100 && min_d <= Math.pow(scale(1.0 * g_onh_key_radius),2)) {
+            g_highlighted_sector_in_vf = label;
         } else {
             g_exploding_sector = -1;
+            g_highlighted_sector_in_vf = -1;
         }
         draw_canvas();
     } else if (g_dragging == DRAG_ONH) {
@@ -381,6 +456,11 @@ function handle_clicks(e) {
 
     if (label == REGION_EYE) {
         g_eye = g_eye == EYE_LEFT ? EYE_RIGHT : EYE_LEFT;
+        draw_canvas();
+    } 
+
+    if (label == REGION_PATTERN) {
+        g_pattern = g_pattern == PATTERN_242 ? PATTERN_G : PATTERN_242;
         draw_canvas();
     } 
 }
